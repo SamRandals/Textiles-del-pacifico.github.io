6992714d5411635d64e908f61aa110e22a722a9e
SamRandals <samnanezedu@gmail.com>
2025-07-23 13:14:28 -0500

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/textiles-del-pacifico',
+    base: '/textiles-del-pacifico-gitpage',
   plugins: [react()],
 })
